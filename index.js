5fc4f218780de3142c519d12d45224cde891e69b
Heil Park <heilpark@me.com>
2023-03-06 11:40:31 +0000

    comments

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// start function
 function start () {
     const team = [];
 
